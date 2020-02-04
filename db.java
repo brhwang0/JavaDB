514cf8a08a9753a4e86d769a7d6f5e9b95fd5dfc
brhwang0 <brhwang0@protonmail.com>
2020-02-04 12:42:57 -0500

    Rename file, add user input

--- db.java
+++ db.java
@@ -1,19 +1,34 @@
-import java.sql.*;  
+import java.util.*;
+import java.sql.*;
 
-public class db {  
-	public static void main(String args[]){  
-		try {  
-			Class.forName("com.mysql.cj.jdbc.Driver");  
-			Connection con = DriverManager.getConnection("jdbc:mysql://192.168.0.37:3306/pokemon","Brandon","123");
-			Statement stmt = con.createStatement();
-			ResultSet rs=stmt.executeQuery("select * from pokedex");  
-			while(rs.next()) {
-				System.out.println(rs.getString(1) + "  " + rs.getString(2));
+public class JavaDB {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Please enter the host URL or IP address: ");
+		String host = s.nextLine();
+		System.out.print("Please enter the database name: ");
+		String dbname = s.nextLine();
+		System.out.print("Please enter username: ");
+		String username = s.nextLine();
+		System.out.print("Please enter password: ");
+		String password = s.nextLine();
+		String connection = "jdbc:mysql://" + host + ":3306/" + dbname;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(connection, username, password);
+			//Connection con = DriverManager.getConnection("jdbc:mysql://192.168.0.37:3306/pokemon","HH","123");   
+			Statement stmt = con.createStatement();  
+			ResultSet rs = stmt.executeQuery("select * from pokedex");
+			while (rs.next()) {
+				System.out.println(rs.getString(1) +"  " + rs.getString(2));
 			}
-			con.close();
 		}
 		catch (Exception e) {
 			System.out.println(e);
-		}  
-	}  
-}  
+		}
+		
+	}
+}
